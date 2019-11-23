2b68a644ba0b476597d1d2a478f4fc2b4b48781c
Abigayle Franklin <abifanklin@gmail.com>
2019-11-22 18:27:32 -0600

    Successfully connected to MongoDB

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 //Configure environment variables
 require('dotenv').config();
@@ -12,6 +13,14 @@ const port = process.env.PORT || 5000;
 //Middleware
 app.use(cors());
 app.use(express.json()); 
+
+//Connect to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully')
+})
 
 //Listen for server
 app.listen(port, () => {
