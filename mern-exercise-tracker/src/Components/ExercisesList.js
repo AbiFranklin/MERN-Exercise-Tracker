2d0829ca8af55356d039c42b537d7ff9a912f2e5
Abigayle Franklin <abifanklin@gmail.com>
2019-11-24 12:32:57 -0600

    Exercise list component complete with dummy data

--- mern-exercise-tracker/src/Components/ExercisesList.js
+++ mern-exercise-tracker/src/Components/ExercisesList.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Button, Container, Row, Col } from 'react-bootstrap';
-import bike from '../img/bicycle.png';
-import exercise from '../img/exercise.png';
-import weight from '../img/gym.png';
-import swim from '../img/swimmer.png'
-import yoga from '../img/yoga.png'
 
 
 function ExercisesList() {
@@ -16,7 +11,7 @@ function ExercisesList() {
         "description": "Biking",
         "duration": 30,
         "date": "2019-11-23T03:18:00.752Z",
-        "pic": "../img/bicycle.png",
+        "pic": "http://abigaylefranklin.com/img/bike.png",
         "createdAt": "2019-11-23T20:26:14.318Z",
         "updatedAt": "2019-11-23T20:26:14.318Z",
         "__v": 0
@@ -27,7 +22,7 @@ function ExercisesList() {
         "description": "Aerobics",
         "duration": 30,
         "date": "2019-11-23T03:18:00.752Z",
-        "pic": "../img/bicycle.png",
+        "pic": "http://abigaylefranklin.com/img/exercise.png",
         "createdAt": "2019-11-23T20:27:29.936Z",
         "updatedAt": "2019-11-23T20:27:29.936Z",
         "__v": 0
@@ -38,7 +33,7 @@ function ExercisesList() {
         "description": "Weights",
         "duration": 15,
         "date": "2019-11-23T03:18:00.752Z",
-        "pic": "../img/bicycle.png",
+        "pic": "http://abigaylefranklin.com/img/weight.png",
         "createdAt": "2019-11-23T20:27:54.082Z",
         "updatedAt": "2019-11-23T20:27:54.082Z",
         "__v": 0
@@ -49,7 +44,7 @@ function ExercisesList() {
         "description": "Running",
         "duration": 20,
         "date": "2019-11-23T03:18:00.752Z",
-        "pic": "../img/bicycle.png",
+        "pic": "http://abigaylefranklin.com/img/run.png",
         "createdAt": "2019-11-23T20:28:16.633Z",
         "updatedAt": "2019-11-23T20:28:16.633Z",
         "__v": 0
@@ -60,7 +55,7 @@ function ExercisesList() {
         "description": "Swimming",
         "duration": 40,
         "date": "2019-11-23T03:18:00.752Z",
-        "pic": "../img/bicycle.png",
+        "pic": "http://abigaylefranklin.com/img/swim.png",
         "createdAt": "2019-11-23T20:28:38.648Z",
         "updatedAt": "2019-11-23T20:28:38.648Z",
         "__v": 0
@@ -71,7 +66,7 @@ function ExercisesList() {
         "description": "Yoga",
         "duration": 30,
         "date": "2019-11-23T03:18:00.752Z",
-        "pic": "../img/bicycle.png",
+        "pic": "http://abigaylefranklin.com/img/yoga.png",
         "createdAt": "2019-11-23T20:28:54.163Z",
         "updatedAt": "2019-11-23T20:28:54.163Z",
         "__v": 0
@@ -86,16 +81,16 @@ function ExercisesList() {
       <Container fluid={true}>
         <Row>
             {exercises.map(exercise => { return (
-              <Col xs={12} sm={8} md={7} lg={6} xl={4}>
+              <Col xs={12} sm={{ span:8, offset: 2 }} md={7} lg={3} xl={{span: 2, offset: 0}}>
               <Card className='card' >
-              <Card.Img variant="top" src='https://picsum.photos/200' />
-              <Card.Body>
-                <Card.Title>{exercise.description}</Card.Title>
+              <Card.Img src={exercise.pic} />
+              <Card.Body className='cardbody'>
+                <Card.Title >{exercise.description}</Card.Title>
                 <Card.Text>
-                  <p>User: {exercise.username}</p>
-                  <p>Duration: {exercise.duration}</p>
+                  <p>User: <br/>{exercise.username}</p>
+                  <p>Duration: <br/>{exercise.duration}</p>
                 </Card.Text>
-                <Button variant="primary">Edit</Button>
+                <Button className='editbutton' variant="secondary">Edit</Button>
               </Card.Body>
             </Card>
             </Col>
